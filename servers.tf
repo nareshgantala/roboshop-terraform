69c2e2df312eab2e2f54037f89d700b80d398a23
nareshgantala <nareshgantala@gmail.com>
2024-10-10 14:56:41 +0530

    for_each

--- servers.tf
+++ servers.tf
@@ -79,6 +79,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
+  for_each = var.instances
   zone_id = "Z0795361K3CL8LSW1B54"
   name    = "${each.value["name"]}-dev.cloudlife.site"
   type    = "A"
