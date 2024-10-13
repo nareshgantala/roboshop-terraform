5001c50cb7cdc41a313bc3496e874f9aa98e97d2
nareshgantala <nareshgantala@gmail.com>
2024-10-13 12:35:45 +0530

    push module

---
+++ module/main.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.Centos8.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = var.componenet_name
+  }
+}
+
+
+resource "null_resource" "provisioner" {
+  depends_on = [ aws_instance.frontend, aws_route53_record.frontend ]
+  for_each = var.components
+  provisioner "remote-exec" {
+  
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = aws_instance.instance.private_ip
+  }
+  
+  inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/nareshgantala/roboshop-shell.git ",
+      "cd roboshop-shell",
+      "sudo bash ${var.componenet_name}.sh ${var.password}
+  ]
+}
+}
+
+
+resource "aws_route53_record" "records" {
+  zone_id = "Z05602471NCWAU8HMW8C"
+  name    = "${var.var.componenet_name}-dev.cloudlife.site"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.instance.private_ip]
+}
+
--- servers.tf
+++ servers.tf
@@ -1,44 +1,8 @@
-resource "aws_instance" "frontend" {
-  # count = length(var.instances)
+module "servers" {
   for_each = var.components
-  ami           = data.aws_ami.Centos8.image_id
+
+  source = "./module"
+  componenet_name = each.value["name"]
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = each.value["name"]
-  }
+  password = lookup(each.value, "password", "null" )
 }
-
-
-resource "null_resource" "provisioner" {
-  depends_on = [ aws_instance.frontend, aws_route53_record.frontend ]
-  for_each = var.components
-  provisioner "remote-exec" {
-  
-  connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = aws_instance.frontend[each.value["name"]].private_ip
-  }
-  
-  inline = [
-      "rm -rf roboshop-shell",
-      "git clone https://github.com/nareshgantala/roboshop-shell.git ",
-      "cd roboshop-shell",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null")}"
-  ]
-}
-}
-
-
-resource "aws_route53_record" "frontend" {
-  for_each = var.components
-  zone_id = "Z05602471NCWAU8HMW8C"
-  name    = "${each.value["name"]}-dev.cloudlife.site"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.frontend[each.value["name"]].private_ip]
-}
-
