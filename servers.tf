4f66ca9c9989684632423ef92e4c1bf991ffd74c
nareshgantala <nareshgantala@gmail.com>
2024-10-13 13:55:04 +0530

    fix

--- servers.tf
+++ servers.tf
@@ -1,7 +1,6 @@
 module "servers" {
-  for_each = var.components
-
   source = "./module"
+  for_each = var.components
   env = var.env
   component_name = each.value["name"]
   instance_type = each.value["instance_type"]
