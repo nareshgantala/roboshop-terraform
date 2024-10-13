9d441aa01c183d541e59603603e4c6c46aa1fdfd
nareshgantala <nareshgantala@gmail.com>
2024-10-13 16:46:25 +0530

    fix

--- servers.tf
+++ servers.tf
@@ -1,8 +1,8 @@
 module "servers" {
+  components = var.components
   for_each = var.components  
   source = "./module"
   component_name = each.value["name"]
-  components = var.components
   env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null" )
