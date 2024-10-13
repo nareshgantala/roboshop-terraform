18f2ac9490a71ca57bb065c7a96969dcc1c6876a
nareshgantala <nareshgantala@gmail.com>
2024-10-13 16:45:23 +0530

    componenets added

--- servers.tf
+++ servers.tf
@@ -2,6 +2,7 @@ module "servers" {
   for_each = var.components  
   source = "./module"
   component_name = each.value["name"]
+  components = var.components
   env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null" )
