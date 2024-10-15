8eb8703f7b778ce6dc9fbbaff5d49dfe6023bde1
nareshgantala <nareshgantala@gmail.com>
2024-10-15 11:54:07 +0530

    push

--- servers.tf
+++ servers.tf
@@ -2,17 +2,18 @@ module "database-servers" {
   for_each = var.database_servers  
   source = "./module"
   component_name = each.value["name"]
-  # env = var.env
+  env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null" )
-  
+  provisioner = true
 }
 
 module "app-servers" {
+  depends_on = [ module.database-servers ]
   for_each = var.app_servers  
   source = "./module"
   component_name = each.value["name"]
-  # env = var.env
+  env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null" )
   
@@ -21,8 +22,6 @@ module "app-servers" {
 
 
 
-variable "components" {}
-variable "env" {}
 
 
 
---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "database_servers" {}
+variable "app_servers" {}
+variable "env" {
+  
+}
+
